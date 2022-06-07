8941e7c229a74b1d54df8271604f10886d1e1f70
vishalcs106 <vishalcs106@rocketmail.com>
2022-06-07 23:22:14 +0530

    Added auth

---
+++ auth/authorization.js
@@ -0,0 +1,20 @@
+module.exports = (admin) => {
+  return (req, res, next) => {
+    const token = req.headers["authorization"];
+    console.log("token"+token)
+    if (!token) {
+      return res.status(401).send("401 : Unauthorized");
+    } else {
+      admin
+        .auth()
+        .verifyIdToken(token)
+        .then(() => {
+          next();
+        })
+        .catch((err) => {
+            console.log(err)
+          res.status(401).send("401 : Unauthorized");
+        });
+    }
+  };
+};
--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const games = require("./api/games");
+const admin = require('firebase-admin')
+const serviceAccount = require("./firebase-key.json");
+const authorize = require("./auth/authorization");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://cute-mouse.firebaseio.com"
+});
+
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/games", games);
+app.use("/api/games", authorize(admin), games);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
